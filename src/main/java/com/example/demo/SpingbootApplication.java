421daa5475d8fbf137d187a9ece5d85e82bd9fe0
chentianhao <chentianhao@360.net>
2018-10-08 14:46:10 +0800

    zuul整合filter类，不能测试（未成品）

--- src/main/java/com/example/demo/SpingbootApplication.java
+++ src/main/java/com/example/demo/SpingbootApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+//@EnableZuulProxy
 public class SpingbootApplication {
 
 	public static void main(String[] args) {
